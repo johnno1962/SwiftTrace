c97491adbf5239b0736b073716226afbac011cc6
John Holdsworth <github@johnholdsworth.com>
2016-06-11 14:04:53 +0100

    armv7 trampoline

--- SwiftTrace/xt_forwarding_trampoline_arm7.s
+++ SwiftTrace/xt_forwarding_trampoline_arm7.s
@@ -5,7 +5,7 @@
 
 # Write out the trampoline table, aligned to the page boundary
 .text
-.align 12
+.align 14
 .globl _xt_forwarding_trampoline_page
 .globl _xt_forwarding_trampolines_start
 .globl _xt_forwarding_trampolines_next
@@ -16,15 +16,15 @@ _xt_forwarding_trampoline:
 	push {r7, lr}           // save frame potinter and return addresss
 	mov	r7, sp              // set up new frame
     push {r0, r1, r2, r3, r9}   // save first four args on stack
-    sub r12, #0x4000        // r12 = r12 - 4096, that is where the data for this trampoline is stored
+    sub r12, #0x4000        // r12 = r12 - pagesize, that is where the data for this trampoline is stored
     ldr r0, [r12, #-4]      // first arg is user data ptr
     ldr r12, [r12]          // get potinter to hander func
     blx r12                 // call tracer function
-    mov r12, r0
+    mov r12, r0             // return value is original implementation
     pop {r0, r1, r2, r3, r9}
 	mov	sp, r7              // unwind stack
     pop {r7, lr}
-    mov pc, r12
+    mov pc, r12             // pass control to original imp.
 
 _xt_forwarding_trampolines_start:
 # Save pc+8 into r12, then jump to the actual trampoline implementation
