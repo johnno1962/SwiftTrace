8e08b280722167ffa7d69d4601ffc9a4d162ee1e
John Holdsworth <github@johnholdsworth.com>
2020-05-05 11:54:20 +0200

    Gitastrophie

--- SwiftTrace/SwiftArgs.swift
+++ SwiftTrace/SwiftArgs.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 John Holdsworth. All rights reserved.
 //
 //  Repo: https://github.com/johnno1962/SwiftTrace
+//  $Id: //depot/SwiftTrace/SwiftTrace/SwiftArgs.swift#50 $
 //
 //  Decorate trace with argument/return values
 //  ==========================================
