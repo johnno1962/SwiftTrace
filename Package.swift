a29f408cb1419b30d1d8f6e0b5ae57aa194fba26
John Holdsworth <github@johnholdsworth.com>
2021-03-09 09:35:36 +0100

    Move to os_unfair_lock

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  Repo: https://github.com/johnno1962/SwiftTrace
-//  $Id: //depot/SwiftTrace/Package.swift#5 $
+//  $Id: //depot/SwiftTrace/Package.swift#7 $
 //
 
 import PackageDescription
